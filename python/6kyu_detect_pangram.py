b759d51a0dfd55b7e4431c9b0e5a34b82e846bf1
Anssi Ahola <anssia.ahola@gmail.com>
2021-03-24 08:02:34 +0200

    update 6kyu_detect_pangram.py

--- python/6kyu_detect_pangram.py
+++ python/6kyu_detect_pangram.py
@@ -1,6 +1,13 @@
+import string
+
+
 def is_pangram(s):
     clean = ''.join([c.lower() if c.isalpha() else '' for c in list(s)])
     return len(set(clean)) == 26
+
+# Taken from Solutions for this kata
+def is_pangram(s):
+    return set(string.ascii_lowercase) <= set(s.lower())
 
 
 print(is_pangram("The quick, brown fox jumps over the lazy dog!"), True)
