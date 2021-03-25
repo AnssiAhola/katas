dfcb7ddc64fa0cd1837da814f85757ec7773180f
Anssi Ahola <anssia.ahola@gmail.com>
2021-03-25 06:45:13 +0200

    7 kyu - Mumbling (Python)

---
+++ python/7kyu_mumbling.py
@@ -0,0 +1,24 @@
+def accum(s):
+    output = ''
+    for i, c in enumerate(s):
+        if i != 0:
+            output += '-'
+        output += c.upper() + c.lower()*i
+    return output
+
+
+# From Kata Solutions
+def accum(s):
+    return '-'.join(c.upper() + c.lower()*i for i, c in enumerate(s))
+
+
+print(accum("ZpglnRxqenU") ==
+      "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+print(accum("NyffsGeyylB") ==
+      "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+print(accum("MjtkuBovqrU") ==
+      "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+print(accum("EvidjUnokmM") ==
+      "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+print(accum("HbideVbxncC") ==
+      "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
